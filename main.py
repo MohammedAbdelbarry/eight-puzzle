914e13cc70448f859c3a2f121cf86309919741f0
Mohamed A. Tolba <tolba96mohamed@gmail.com>
2018-10-27 23:28:55 +0200

    Added Maximum Depth and Running Time Calculations

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from puzzle import PuzzleProblem, PuzzleState
 from search import bfs, dfs, astar, ucs
 from heuristic import manhattan_distance_heuristic, euclidean_distance_heuristic
 from visualizer import Visualizer
+import time
 
 
 if __name__ == '__main__':
-    puzzle = PuzzleProblem(PuzzleState([[1, 0, 2], [3, 4, 5], [6, 7, 8]]))
+    puzzle = PuzzleProblem(PuzzleState([[3, 1, 2], [4, 0, 5], [6, 7, 8]]))
     #puzzle = PuzzleProblem()
+    print("Initial Puzzle State")
+    print()
     print(puzzle.puzzle)
-    path, explored_states_count = bfs(puzzle)
+    start_time = time.time()
+    path, explored_states_count, maximum_depth_reached = bfs(puzzle)
+    end_time = time.time()
     if not path:
         print('No solution was found for the puzzle:\n%s' % str(puzzle.puzzle))
         exit(1)
     print()
     print("Number of explored states: %d" % explored_states_count)
     print()
-    print("Cost of the solution: " + str(len(path) - 1))
+    print("Cost of the solution: %d" % (len(path) - 1))
+    print()
+    print("Maximum depth reached: %d" % maximum_depth_reached)
+    print()
+    print("Running time(seconds): %f" % (end_time - start_time))
     Visualizer(path).play()
 
--- search.py
+++ search.py
@@ -6,13 +6,17 @@ from util import Stack, Queue, PriorityQueue
 def bfs_dfs(problem, frontier):
     parent = {}
     explored = set()
+    depth = {}
     explored_states_count = 0
+    maximum_depth_reached = -1
     frontier.push(problem.get_initial_state())
     parent[problem.get_initial_state()] = None
+    depth[problem.get_initial_state()] = 0
     explored.add(problem.get_initial_state())
     while not frontier.is_empty():
         cur = frontier.pop()
         explored_states_count += 1
+        maximum_depth_reached = max(maximum_depth_reached, depth[cur])
         if problem.is_goal_state(cur):
             p = cur
             path = []
@@ -20,30 +24,34 @@ def bfs_dfs(problem, frontier):
                 path.append(p)
                 p = parent[p]
             path.reverse()
-            return path, explored_states_count
+            return path, explored_states_count, maximum_depth_reached
         for next_state in problem.get_neighbors(cur):
             state = next_state[0]  
             if state not in explored:
                 parent[state] = cur
+                depth[state] = depth[cur] + 1
                 frontier.push(state)
                 explored.add(state)
                 
-    return [], explored_states_count
+    return [], explored_states_count, maximum_depth_reached
         
 
 def ucs_astar(problem, frontier, heuristic):
     parent = {}
     cost = {}
+    depth = {}
     explored = set()
     explored_states_count = 0
+    maximum_depth_reached = -1
     frontier.push(problem.get_initial_state(), 0 + heuristic(problem.get_initial_state()))
     parent[problem.get_initial_state()] = None
     cost[problem.get_initial_state()] = 0
-    
+    depth[problem.get_initial_state()] = 0
     while not frontier.is_empty():
         cur = frontier.pop()
         explored.add(cur)
         explored_states_count += 1
+        maximum_depth_reached = max(maximum_depth_reached, depth[cur])
         if problem.is_goal_state(cur):
             p = cur
             path = []
@@ -51,20 +59,22 @@ def ucs_astar(problem, frontier, heuristic):
                 path.append(p)
                 p = parent[p]
             path.reverse()
-            return path, explored_states_count
+            return path, explored_states_count, maximum_depth_reached
         for next_state in problem.get_neighbors(cur):
             state = next_state[0]
             if state not in cost:
                 parent[state] = cur
+                depth[state] = depth[cur] + 1 
                 cost[state] = cost[cur] + next_state[2]
                 frontier.push(state, cost[state] + heuristic(state))
             else:
                 if cost[state] > cost[cur] + next_state[2]:
                     parent[state] = cur
+                    depth[state] = depth[cur] + 1
                     cost[state] = cost[cur] + next_state[2]
                     frontier.update(state, cost[state] + heuristic(state))
 
-    return [], explored_states_count
+    return [], explored_states_count, maximum_depth_reached
 
 
 def bfs(problem: SearchProblem) -> [List[SearchState], int]:
