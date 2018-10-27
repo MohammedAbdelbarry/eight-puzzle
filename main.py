0cd86d0255a923b4b0b46d90b289395a0cfd6997
Mohammed Abdelbarry <m7md337@gmail.com>
2018-10-27 06:27:37 +0200

    Check for unsolvable puzzles, and fix a bug with animation

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from visualizer import Visualizer
 
 
 if __name__ == '__main__':
-    puzzle = PuzzleProblem(PuzzleState([[3, 0, 6], [7, 8, 2], [4, 1, 5]]))
-    #puzzle = PuzzleProblem()
-    print(puzzle.puzzle)
-    path, explored_states_count = bfs(puzzle)
-    print("Number of explored states: " + str(explored_states_count) + "\n")
+    puzzle = PuzzleProblem(width=3, height=3)
+    path, explored_states_count = dfs(puzzle)
+    if not path:
+        print('No solution was found for the puzzle:\n%s' % str(puzzle.puzzle))
+        exit(1)
+    print()
+    print("Number of explored states: %d" % explored_states_count)
+    print()
     print("Cost of the solution: " + str(len(path) - 1))
     Visualizer(path).play()
 
